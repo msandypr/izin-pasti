3f88bb7f564e032e7ea5c6bcaa63501d3f39122a
msandypr <msandypr.dev@gmail.com>
2025-05-01 20:24:21 +0700

    fix: header

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ import WhatsappLogo from "@/components/etc/WhatsappLogo";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-white/95 backdrop-blur">
+    <header className="sticky top-0 z-50 w-full border-b bg-gradient-to-b from-[#2CBCC4]/10 to-white backdrop-blur">
       <div className="container flex h-16 items-center justify-between">
         <div className="flex items-center gap-2">
           <Link href="/" className="flex items-center">
